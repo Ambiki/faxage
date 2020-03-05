a633d16ef687904e0b43fc414c088b44ad3aa678
Kevin Dias <diasks2@gmail.com>
2020-03-06 07:20:47 +0900

    Update listfax output

--- lib/faxage/receive_fax.rb
+++ lib/faxage/receive_fax.rb
@@ -42,7 +42,68 @@ module Faxage
       elsif response.parsed_response.include?("ERR11: No incoming faxes available")
         raise NoIncomingFaxesError.new("There are no incoming faxes to list for you.")
       else
-        return response.parsed_response
+        data = []
+        response.parsed_response.split("\n").each do |received_fax|
+          # <recvid><tab><recvdate>(OPTIONAL:
+          # <tab><starttime>)
+          # <tab><CID><tab><DNIS>(OPTIONAL:
+          # <tab><filename>)(OPTIONAL:
+          # <tab><pagecount>)(OPTIONAL: <tab><tsid>)
+          individual_fax = Hash.new
+          received_fax.split("\t").each_with_index do |item, index|
+            if options[:starttime].nil?
+              case index
+              when 0
+                individual_fax[:recvid] = item
+              when 1
+                individual_fax[:revdate] = item
+              when 2
+                individual_fax[:cid] = item
+              when 3
+                individual_fax[:dnis] = item
+              when 4 && !options[:filename].nil?
+                individual_fax[:filename] = item
+              when 4 && options[:filename].nil? && !options[:pagecount].nil?
+                individual_fax[:pagecount] = item
+              when 4 && options[:filename].nil? && options[:pagecount].nil? && !options[:tsid].nil?
+                individual_fax[:tsid] = item
+              when 5 && !options[:filename].nil? && !options[:pagecount].nil?
+                individual_fax[:pagecount] = item
+              when 5 && !options[:filename].nil? && options[:pagecount].nil? && !options[:tsid].nil?
+                individual_fax[:tsid] = item
+              when 6 && !options[:filename].nil? && !options[:pagecount].nil? && !options[:tsid].nil?
+                individual_fax[:tsid] = item
+              end
+            else
+              case index
+              when 0
+                individual_fax[:recvid] = item
+              when 1
+                individual_fax[:revdate] = item
+              when 2
+                individual_fax[:starttime] = item
+              when 3
+                individual_fax[:cid] = item
+              when 4
+                individual_fax[:dnis] = item
+              when 5 && !options[:filename].nil?
+                individual_fax[:filename] = item
+              when 5 && options[:filename].nil? && !options[:pagecount].nil?
+                individual_fax[:pagecount] = item
+              when 5 && options[:filename].nil? && options[:pagecount].nil? && !options[:tsid].nil?
+                individual_fax[:tsid] = item
+              when 6 && !options[:filename].nil? && !options[:pagecount].nil?
+                individual_fax[:pagecount] = item
+              when 6 && !options[:filename].nil? && options[:pagecount].nil? && !options[:tsid].nil?
+                individual_fax[:tsid] = item
+              when 7 && !options[:filename].nil? && !options[:pagecount].nil? && !options[:tsid].nil?
+                individual_fax[:tsid] = item
+              end
+            end
+            data << individual_fax
+          end
+        end
+        return data
       end
     end
   end
